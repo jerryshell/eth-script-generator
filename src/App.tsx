93fb21bc4ce12cd84c2f37690b78546d8dc9d397
Jerry <calljerryli@outlook.com>
2021-03-26 17:16:18 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                className="window"
+                style={{width: '600px', margin: '0 auto'}}
+            >
+                <div className="title-bar">
+                    <div className="title-bar-text">
+                        ETH Script Generator
+                    </div>
+                </div>
+                <div className="window-body">
+                    <div className="field-row-stacked">
+                        <label>Miner filepath</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field-row-stacked">
+                        <label>Fake name</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field-row-stacked">
+                        <label>Config</label>
+                        <textarea rows={8}/>
+                    </div>
+                    <div className="field-row-stacked">
+                        <label>Config file name</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field-row-stacked">
+                        <label>Kill at</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="field-row-stacked">
+                        <label>Script</label>
+                        <textarea rows={8}/>
+                    </div>
+                    <div className="field-row">
+                        <button>Copy</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
